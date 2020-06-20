8f607fd2ba3efe66876a968b63954ad14d87a15c
EC2 Default User <ec2-user@ip-172-31-43-82.ap-northeast-1.compute.internal>
2020-06-20 14:04:00 +0000

    課題18 修正1

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -12,8 +12,8 @@
 
     @section('content')
         <div class="container">
-            <hr color="#c0c0c0">
             <h1>ニュース一覧</h1>
+            <hr color="#c0c0c0">
             @if (!is_null($headline))
                 <div class="row">
                     <div class="headline col-md-10 mx-auto">
